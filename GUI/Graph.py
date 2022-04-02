a1fad16ec9f874cd8518db43d0a162f87f5a6262
Evan Dyce <endyce@gmail.com>
2022-04-01 22:20:10 -0700

    Adds functions for left, right, and mid riemann

--- GUI/Graph.py
+++ GUI/Graph.py
@@ -1,8 +1,12 @@
 import tkinter as tk
+from tracemalloc import start
+from turtle import width
+from matplotlib.axes import Axes
 from matplotlib.figure import Figure
 from matplotlib.backends.backend_tkagg import (FigureCanvasTkAgg, NavigationToolbar2Tk)
 from matplotlib.pyplot import plot_date
 from functions import Functions
+import numpy as np
 import globals
 
 
@@ -16,19 +20,7 @@ class Graph(tk.Frame):
 
         self.canvas = None
         self.fig = None
-        self.plot('Linear')
-
-    def clear(self, name):
-        print('cleared')
-        self.fig.clear(True)
-        for item in self.winfo_children():
-            item.destroy()
-
-        self.plot(name)
-
-
-    def plot(self, function):
-        func_dict = {
+        self.func_dict = {
             'Linear': Functions.Linear,
             'Quadratic': Functions.Quadratic, 
             'Cubic': Functions.Cubic,
@@ -38,9 +30,51 @@ class Graph(tk.Frame):
             'Cos': Functions.Cos,
             'Tan': Functions.Tan
         }
+        self.plot('Linear')
 
-        fig = func_dict[function]()
+    def clear(self, name):
+        self.fig.clear(True)
+        for item in self.winfo_children():
+            item.destroy()
+
+        self.plot(name)
+
+
+    def plot(self, function):
+        func = self.func_dict[function]()
+        domain = Functions.get_domain(func)
+        fig, ax = Functions.generate(x=func[0], y=func[1], domain=domain)
         self.fig = fig
+
+        self.midpoint_riemann(5, function, ax)
         self.canvas = FigureCanvasTkAgg(fig, master=self)
         self.canvas.draw()
         self.canvas.get_tk_widget().pack(fill=tk.BOTH, expand=tk.YES, padx=50)
+
+    
+    def left_riemann(self, n, function, ax: Axes):
+        domain = Functions.get_domain(function)
+        x = np.arange(start=domain[0], stop=domain[1]+1, step=(domain[1]-domain[0])/n)
+        y = self.func_dict[function](x=x)[1]
+        x_left = x[:-1]
+        y_left = y[:-1]
+
+        ax.bar(x_left,y_left,width=(domain[1]-domain[0])/n,alpha=0.2,align='edge',edgecolor='b')
+
+    def right_riemann(self, n, function, ax: Axes):
+        domain = Functions.get_domain(function)
+        x = np.arange(start=domain[0], stop=domain[1]+1, step=(domain[1]-domain[0])/n)
+        y = self.func_dict[function](x=x)[1]
+        x_right = x[1:]
+        y_right = y[1:]
+
+        ax.bar(x_right, y_right, width=-(domain[1]-domain[0])/n, alpha=0.2, align='edge', edgecolor='b')
+
+    def midpoint_riemann(self, n, function, ax: Axes):
+        domain = Functions.get_domain(function)
+        x = np.arange(start=domain[0], stop=domain[1]+1, step=(domain[1]-domain[0])/n)
+        x_mid = (x[1:]+x[:-1])/2
+        print(x_mid)
+        y_mid = self.func_dict[function](x=x_mid)[1]
+
+        ax.bar(x_mid, y_mid, width=(domain[1]-domain[0])/n, alpha=0.2, edgecolor='b')

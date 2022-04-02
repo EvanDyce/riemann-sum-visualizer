f7f2e5b53509ad980be9cc9ca1480f2957b9c0d8
Evan Dyce <endyce@gmail.com>
2022-04-01 17:28:01 -0700

    reworked functiosn to take domain and return x, y

--- functions.py
+++ functions.py
@@ -5,61 +5,77 @@ import numpy as np
 
 
 class Functions():
+
     @staticmethod
-    def generate(x, y, domain):
+    def get_domain(function):
+        if function == 'Exponential':
+            return (-1, 5)
+        elif function == 'Logarithmic':
+            return (0.001, 5)
+        else:
+            return (-5, 5)
+
+        
+    @staticmethod
+    def generate(x, y, domain): 
         fig = plt.Figure()
         ax = fig.add_subplot(1, 1, 1)
         ax.xaxis.set_ticks_position('bottom')
         ax.yaxis.set_ticks_position('left')
         ax.hlines(y=0, color='r', xmin=domain[0], xmax=domain[1])
         ax.plot(x, y)
-        return fig
+        
+        return (fig, ax)
 
+    # @staticmethod
+    # def left_riemann(n, function, fig):
+    #     domain = Functions.get_domain(function)
+
+    #     x = np.linspace(domain[0], domain[1], n)
+    #     y = self.func_dict[function](x=x)[1]
+    #     x_left = x[:-1]
+    #     y_left = y[:-1]
+    #     ax = fig.add_subplot(1, 1, 1, sharex=fig.get_axes()[0], sharey=fig.get_axes()[0])
+
+    #     ax.bar(x_left,y_left,width=(domain[1]-domain[0])/n,alpha=0.2,align='edge',edgecolor='b')
 
     @staticmethod
-    def Linear():
-        x = np.linspace(-5, 5, 200)
+    def Linear(x=np.linspace(-5, 5, 200)):
         y = x
-        return Functions.generate(x, y, (-5, 5))
+        return (x, y)
 
     @staticmethod
-    def Quadratic():
-        x = np.linspace(-5, 5, 200)
+    def Quadratic(x=np.linspace(-5, 5, 200)):
         y = x**2
-        return Functions.generate(x, y, (-5, 5))
+        return (x, y)
 
     @staticmethod
-    def Cubic():
-        x = np.linspace(-5, 5, 200)
+    def Cubic(x=np.linspace(-5, 5, 200)):
         y = x**3
-        return Functions.generate(x, y, (-5, 5))
+        return (x, y)
 
     @staticmethod
-    def Exponential():
-        x = np.linspace(-1, 5, 200)
+    def Exponential(x=np.linspace(-1, 5, 200)):
         y = 2**x
-        return Functions.generate(x, y, (-1, 5))
+        return (x, y)
 
     @staticmethod
-    def Logarithm():
-        x = np.linspace(0.001, 5, 200)
+    def Logarithm(x=np.linspace(0.001, 5, 200)):
         y = [log(num) for num in x]
-        return Functions.generate(x, y, (0, 5))
+        return (x, y)
 
     @staticmethod
-    def Sin():
-        x = np.linspace(-5, 5, 200)
+    def Sin(x=np.linspace(-5, 5, 200)):
         y = [sin(num) for num in x]
-        return Functions.generate(x, y, (-5, 5))
+        return (x, y)
 
     @staticmethod
-    def Cos():
-        x = np.linspace(-5, 5, 200)
+    def Cos(x=np.linspace(-5, 5, 200)):
         y = [cos(num) for num in x]
-        return Functions.generate(x, y, (-5, 5))
+        return (x, y)
 
     @staticmethod
-    def Tan():
-        x = np.linspace(-5, 5, 200)
+    def Tan(x=np.linspace(-5, 5, 200)):
         y = [tan(num) for num in x]
-        return Functions.generate(x, y, (-5, 5))
+        return (x, y)
+
